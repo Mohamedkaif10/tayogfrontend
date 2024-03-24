cacb8dc5b5211a49c88ec92434b995a63a937376
Vivek <365anu65@gmail.com>
2024-03-24 16:48:30 +0530

    Mentor page is done...

--- tayogfrontend/src/app/mentor/page.js
+++ tayogfrontend/src/app/mentor/page.js
@@ -3,6 +3,12 @@ import Navbar from "@/components/Navbar";
 import React from "react";
 import HeroBanner from "./HeroBanner";
 import InfiniteScroll from "./InfiniteScroll";
+import Disciplines from "./Disciplines";
+import TopInstitutes from "./TopInstitutes";
+import Bannering from "./Bannering";
+import Empowering from "./Empowering";
+import Evidence from "./Evidence";
+import Quotation from "./Quotation";
 
 const MentorHome = () => {
   return (
@@ -10,6 +16,12 @@ const MentorHome = () => {
       <Navbar />
       <HeroBanner />
       <InfiniteScroll />
+      <Disciplines />
+      <TopInstitutes />
+      <Bannering />
+      <Empowering />
+      <Evidence />
+      <Quotation />
       <Footer />
     </div>
   );
